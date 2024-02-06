090ceb43c3c1aabe5ad1164f9746093c7da68c9d
Rohan <grohan1130@gmail.com>
2024-02-06 15:57:45 -0600

    Problem Set 0 Completed

---
+++ PS0/ps0.py
@@ -0,0 +1,20 @@
+"""""
+Rohan Gupta
+MIT OCW - 6.0001
+Problem Set 0
+"""""
+
+# importing packages
+import numpy as np
+
+# prompt use inputs
+x = int(input("Enter number x:"))
+y = int(input("Enter number y:"))
+
+# calculations
+z = x**y
+logx = np.log2(x)
+
+# outputs
+print("x**y = ", z)
+print ("log(x) = ", logx)
